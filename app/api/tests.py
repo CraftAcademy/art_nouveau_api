5e94a03f881b4a6000dc91bdc3a5c131bff77896
tochman <thomas@craftacademy.se>
2022-08-16 10:42:52 +0200

    WIP first spec attepmt

--- app/api/tests.py
+++ app/api/tests.py
@@ -1,3 +1,17 @@
+from urllib import request
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import force_authenticate
+
+factory = APIRequestFactory()
+user = User.objects.get(
+    username='test')
+
+
+def test_response(self):
+    request = factory.get('/users/')
+    force_authenticate(request, user=user)
+    response = view(request)
