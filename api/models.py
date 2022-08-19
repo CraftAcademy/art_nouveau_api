a50ec8dbafe1cdb93f2f27f73d815511b012b20e
maxarvid <max.arvid.anderson@gmail.com>
2022-08-19 10:49:39 +0200

    adds attributes to Project model

--- api/models.py
+++ api/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
-# Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return self.title
 
-    # class Meta:
-    #     ordering = ['-created_at']
-    #     verbose_name = 'Project'
-    #     verbose_name_plural = 'Projects'
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
