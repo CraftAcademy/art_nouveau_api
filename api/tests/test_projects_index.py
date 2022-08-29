83b0f01d308f14d44a588883467ce931cddbb7ce
Thomas Ochman <thomas@craftacademy.se>
2022-08-29 10:43:41 +0200

    API respondes with data wrapped in correct resource (#7)
    * adds setting to wrap the list with resource name
    
    * updates test to reflect new json structure

--- api/tests/test_projects_index.py
+++ api/tests/test_projects_index.py
@@ -10,6 +10,7 @@ class ProjectsIndexTest(APITestCase):
         response = self.client.get('http://testserver/projects/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['title'], 'Foo')
-        self.assertEqual(response.data[0]['description'], 'Bar')
-        self.assertTrue(response.data[0]['id'])
+        project = response.data['projects'][0]
+        self.assertEqual(project['title'], 'Foo')
+        self.assertEqual(project['description'], 'Bar')
+        self.assertTrue(project['id'])
